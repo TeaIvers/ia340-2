622469e2d1ac342f2ea3884a9607f0e02e7cb370
TeaIvers <122939351+TeaIvers@users.noreply.github.com>
2023-09-13 18:04:19 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -20,3 +20,31 @@ q2.3
 	--('c2','mongodb','r2','p2@jmu.edu'),
 	--('c3','twitter','r1','p1@jmu.edu')
 select * from course
+
+q2.4
+ --insert into enroll_list (s_email,c_number)
+ --values ('s1@jmu.edu','c1'),
+ 	--('s2@jmu.edu','c1'),
+	--('s3@jmu.edu','c1'),
+	--('s4@jmu.edu','c2'),
+	--('s2@jmu.edu','c3'),
+	--('s3@jmu.edu','c3') 
+	
+select * from enroll_list
+
+q2.5
+-- insert profesor first because if we dont have p3 we cannot insert a new column 
+--insert into professor(p_email,p_name,office)
+--values ('p3@jmu.edu','p3','03') ;
+
+--insert into course(c_number,c_name,room,p_email)
+--values ('c4', 'facebook','r1','p3@jmu.edu')
+
+q2.6
+--change course first to make sure p1 is not teaching any courses then we can delete p1
+--update course
+--set p_email = 'p3@jmu.edu'
+--where p_email = 'p1@jmu.edu';
+
+--delete from professor
+--where p_email = 'p1@jmu.edu'
