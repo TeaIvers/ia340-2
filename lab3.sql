0e822b2fdf23009865d6b1d9712cc40b190ad3e4
TeaIvers <122939351+TeaIvers@users.noreply.github.com>
2023-09-20 17:45:44 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -48,3 +48,31 @@ q2.6
 
 --delete from professor
 --where p_email = 'p1@jmu.edu'
+
+q2.7
+--select * from enroll_list
+
+q2.8
+--select c_number, count(*) as num_student
+--from enroll_list
+--group by c_number
+--order by num_student desc
+--limit 1
+
+q2.9
+--select professor.p_name,
+	course.c_name
+--from professor
+--inner join course 
+--on professor.p_email = course.p_email
+
+q2.10
+--select professor.p_name,
+	--count(course.c_name) as num_course
+--from professor
+--inner join course
+--on professor.p_email = course.p_email
+--group by professor.p_name
+--order by num_course desc
+--limit 1
+	
